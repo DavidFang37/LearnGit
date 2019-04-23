c7d060cad4bdfde867117c9e80c06ab0705c379f
fang <fang0307@outlook.com>
2019-04-23 22:49:22 +0800

    first commit

---
+++ learngit/learngit/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  learngit
+//
+//  Created by fang on 2019/4/23.
+//  Copyright © 2019 fang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
